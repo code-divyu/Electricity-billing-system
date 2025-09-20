d9f85974ee7f821593a0b744c157ea25a5615704
Divyanshu Upadhyay <divyanshuupadhyay000@gmail.com>
2025-09-20 22:48:20 +0530

    added all files

---
+++ src/electricity/billing/system/Conn.java
@@ -0,0 +1,21 @@
+ 
+package electricity.billing.system;
+
+import java.sql.*;
+
+public class Conn {
+  
+    Connection c;
+    Statement s;
+    Conn() {
+        try {
+            
+        
+        c = DriverManager.getConnection("jdbc:mysql:///ebs", "root", "niet2026");
+        s = c.createStatement();
+        
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
